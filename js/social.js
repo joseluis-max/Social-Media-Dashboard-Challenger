31ed1522a5dcbe90250d92c9b5ecdba3b95d4f6e
José Luis Valdiviezo Peña <luisj.valdiviezo20@gmail.com>
2020-05-04 20:20:03 -0500

    Version Final

--- js/social.js
+++ js/social.js
@@ -1,23 +1,25 @@
 "use strict"; 
-const fondo = document.querySelector('body')
-const header = document.querySelector('header')
-const redes = document.querySelectorAll('.redes')
-const tarjetas = document.querySelectorAll('.tarjeta')
-const subtitle = document.getElementById("subtitle")
-function changeDarkMode() {
-  fondo.classList.toggle('darkmode')
-  header.classList.toggle('headerDark')
-  redes.forEach(r=>{
-    r.classList.toggle('redesDark')
+document.addEventListener('DOMContentLoaded',()=>{
+  const fondo = document.querySelector('body')
+  const header = document.querySelector('header')
+  const redes = document.querySelectorAll('.redes')
+  const tarjetas = document.querySelectorAll('.tarjeta')
+  const subtitle = document.getElementById("subtitle")
+  function changeDarkMode() {
+    fondo.classList.toggle('darkmode')
+    header.classList.toggle('headerDark')
+    redes.forEach(r=>{
+      r.classList.toggle('redesDark')
+    })
+    tarjetas.forEach(t=>{
+      t.classList.toggle('tarjetasDark')
+    })
+    subtitle.classList.toggle('letter_white')
+  }
+  const darkMode = document.getElementById("darkmode")
+  darkMode.addEventListener('click',function(){
+  this.firstElementChild.classList.toggle('drog')
+  this.classList.toggle('background');
+  changeDarkMode()
   })
-  tarjetas.forEach(t=>{
-    t.classList.toggle('tarjetasDark')
-  })
-  subtitle.classList.toggle('letter_white')
-}
-const darkMode = document.getElementById("darkmode")
-darkMode.addEventListener('click',function(){
- this.firstElementChild.classList.toggle('drog')
- this.classList.toggle('background');
- changeDarkMode()
-})
+});
